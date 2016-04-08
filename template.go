b729ae822f8a194fb4edee1d428bec7a55b16ed5
Damien Mathieu <damien@heroku.com>
2016-04-08 18:20:44 +0200

    store the demo repo into a tgz
    So we have it in vendors

--- template.go
+++ template.go
@@ -1,8 +1,12 @@
 package gitest
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 	"path"
 	"path/filepath"
 	"runtime"
@@ -14,14 +18,73 @@ type template struct {
 
 func newTemplate(name string) (*template, error) {
 	_, f, _, _ := runtime.Caller(1)
-	folder, err := filepath.Abs(fmt.Sprintf("%s/data/%s", path.Dir(f), name))
+	c, err := filepath.Abs(fmt.Sprintf("%s/%s.tgz", path.Dir(f), name))
 	if err != nil {
 		return nil, err
 	}
-	_, err = ioutil.ReadDir(folder)
+	folder, err := decompress(c)
 	if err != nil {
 		return nil, err
 	}
 
+	fmt.Println(folder)
 	return &template{folder}, nil
+}
+
+func decompress(tarPath string) (string, error) {
+	file, err := os.Open(tarPath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	fileReader, err := gzip.NewReader(file)
+	if err != nil {
+		return "", err
+	}
+	defer fileReader.Close()
+
+	tarBallReader := tar.NewReader(fileReader)
+
+	destPath, err := ioutil.TempDir("", "decompressed_repo")
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		header, err := tarBallReader.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", err
+		}
+
+		filename := path.Join(destPath, header.Name)
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			err = os.MkdirAll(filename, os.FileMode(header.Mode))
+			if err != nil {
+				return "", err
+			}
+
+		case tar.TypeReg:
+			writer, err := os.OpenFile(filename, os.O_CREATE+os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return "", err
+			}
+
+			io.Copy(writer, tarBallReader)
+			if err != nil {
+				return "", err
+			}
+
+			writer.Close()
+		default:
+			return "", fmt.Errorf("Unable to untar type : %c in file %s", header.Typeflag, filename)
+		}
+	}
+
+	return destPath, nil
 }
